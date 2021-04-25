9b8e39e0997e6cab34bb408b1fa82db3f735f07e
Redouane Jnah <redouanejnah2000@gmail.com>
2021-04-25 14:28:08 +0000

    setup filter part

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -5,7 +5,64 @@ import { getUniqueValues, formatPrice } from '../utils/helpers'
 import { FaCheck } from 'react-icons/fa'
 
 const Filters = () => {
-  return <div>Filters</div>
+  const {
+    filters: {
+      text,
+      collections,
+      size,
+      color,
+      max_price,
+      min_price,
+      price,
+      shipping,
+    },
+    updateFilters,
+    clearFilters,
+    all_products,
+  } = useFilterContext()
+
+  const collections = getUniqueValues(all_products, 'collections')
+  const size = getUniqueValues(all_products, 'size')
+  const colors = getUniqueValues(all_products, 'colors')
+
+  return (
+    <Wrapper>
+      <div className="content">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            name="text"
+            placeholder="search"
+            className="search-input"
+            value={text}
+            onChange={updateFilters}
+          />
+        </form>
+      </div>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.section`
+  .search-input {
+    background: var(--second-clr-3);
+    padding: 0.5rem;
+    border-radius: var(--radius);
+    border-color: transparent;
+    letter-spacing: var(--spacing);
+    outline-color: var(--second-clr);
+
+    &::placeholder {
+      text-transform: capitalize;
+    }
+  }
+
+  @media (min-width: 768px) {
+    .content {
+      position: sticky;
+      top: 1rem;
+    }
+  }
+`
 
 export default Filters
